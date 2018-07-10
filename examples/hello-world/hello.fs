1938c5af4638c2909f2e3b357d13b1e44f5c1573
Tijn Kersjes <tijn@divbyzero.nl>
2018-07-10 23:55:39 +0200

    Add save and load to hello example

--- examples/hello-world/hello.fs
+++ examples/hello-world/hello.fs
@@ -12,14 +12,22 @@ require ibm-font.fs
 : rSCX+! rSCX c@ + rSCX c! ;
 : rSCY+! rSCY c@ + rSCY c! ;
 
+: save-x $A000 c! ;
+: load-x $A000 c@ ;
+
+: save-y $A002 c! ;
+: load-y $A002 c@ ;
+
 : handle-input
   begin
     rDIV c@ [ $FF 8 / ]L < if
       key-state
-      dup k-right and if -5 rSCX+! then
-      dup k-left  and if  5 rSCX+! then
-      dup k-up    and if  5 rSCY+! then
-      dup k-down  and if -5 rSCY+! then
+      dup k-right and if -1 rSCX+! then
+      dup k-left  and if  1 rSCX+! then
+      dup k-up    and if  1 rSCY+! then
+      dup k-down  and if -1 rSCY+! then
+      dup k-select and if rSCX c@ save-x rSCY c@ save-y then
+      dup k-start and if load-x rSCX c! load-y rSCY c! then
       \ If there no key pressed, wait for one
       0= if halt then
     then
